49d8ff6e7e292b5a8bd2f793a92cca1a0d680363
Pradeek444 <113882354+Pradeek444@users.noreply.github.com>
2023-07-13 19:28:29 +0530

    Add files via upload

---
+++ Day 5/pages/about.js
@@ -0,0 +1,54 @@
+import React from "react";
+import "../pages/about.css";
+import Footer from "./footer";
+import Navbar from "./navbar";
+
+function About() {
+  return (
+    <>
+    <div className="ab">
+    <Navbar/>
+    <div className="about">
+    <div className="about-content">
+      <h1>About Us</h1>
+      
+      <br />
+      <p className="prw">
+  
+
+Welcome to our Kitchen Display Website, your one-stop destination for all things related to kitchen designs, ideas, and inspiration. We are passionate about creating functional, beautiful, and efficient kitchens that enhance your cooking experience and bring joy to your home.
+
+At Kitchen Display Website, we understand the importance of a well-designed kitchen. It is not only a space for cooking and preparing meals but also a central hub where families gather, friends socialize, and memories are made. We believe that a kitchen should reflect your personal style and cater to your unique needs, making it a place you love to spend time in.
+
+Our mission is to provide you with a comprehensive resource that empowers you to create your dream kitchen. Whether you are remodeling your existing kitchen or starting from scratch, our website offers a wealth of information, tips, and ideas to guide you through the entire process.
+
+Here's what you can expect from our Kitchen Display Website:<br></br>
+
+1. Inspirational Kitchen Designs: Explore a vast collection of stunning kitchen designs from various styles, including modern, traditional, farmhouse, and more. Get inspired by beautiful visuals and discover innovative ideas that suit your taste and lifestyle.
+<br></br>
+2. Product Reviews and Recommendations: We provide unbiased reviews and recommendations on kitchen appliances, fixtures, and accessories. From high-quality cooktops and ovens to stylish faucets and lighting, we help you make informed decisions to ensure the best functionality and aesthetics for your kitchen.
+<br></br>
+3. Practical Kitchen Organization Tips: A well-organized kitchen enhances efficiency and productivity. We share practical tips and ideas for maximizing storage space, optimizing workflow, and decluttering your kitchen, making it easier to find what you need and keep your kitchen tidy.
+<br></br>
+4. Expert Advice from Design Professionals: Our team of experienced kitchen designers and experts shares their knowledge and expertise through articles, guides, and tutorials. Learn about the latest trends, materials, and design principles to create a kitchen that is not only visually appealing but also functional and durable.
+<br></br>
+5. Interactive Tools and Resources: We offer interactive tools and resources to assist you in the kitchen design process. From virtual kitchen planners to budget calculators, these tools help you visualize your ideas, plan your layout, and estimate costs, ensuring a smooth and well-executed kitchen project.
+<br></br>
+We are committed to providing you with the best possible resources and inspiration to bring your dream kitchen to life. Explore our website, dive into our articles, and embark on your kitchen design journey with confidence. We're here to support you every step of the way.
+<br></br>
+Thank you for choosing Kitchen Display Website. Let's create the kitchen of your dreams!
+      </p>
+     
+
+    
+      <br />
+      </div>
+      
+    </div>
+    </div>
+    <Footer/>
+    </>
+  );
+}
+
+export default About;
---
+++ Day 5/pages/nav.js
@@ -0,0 +1,35 @@
+import React from 'react';
+import Navbar from './navbar';
+import '../pages/nav.css';
+import Footer from './footer';
+import { useNavigate } from 'react-router-dom';
+
+const KitchenDisplay = () => {
+  const navigate = useNavigate();
+  const handleClick= (e) => {
+   navigate('/menu');
+  };
+  return (
+    <>
+      <Navbar />
+      <div className="kitchen-display">
+        <div className="kitchen-display-content">
+        
+          <h1 className="kitchen-display-heading">Welcome to Our Kitchen</h1>
+          <p className="kitchen-display-description">
+            Explore our delicious menu options
+          </p>
+          <button onClick={handleClick} className="order-now-button">Menu</button>
+        </div>
+      </div>
+      <Footer />
+    </>
+  );
+};
+
+export default KitchenDisplay;
+
+
+
+
+
